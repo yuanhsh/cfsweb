9e9528037306754c75fb5d58e905909f4784881c
Yuan Haisheng <yuanhaisheng@bit-ware.co.jp>
2015-01-19 16:05:28 -0500

    changed model

--- src/com/cfs/dao/Model.java
+++ src/com/cfs/dao/Model.java
@@ -9,25 +9,25 @@ import org.genericdao.RollbackException;
 
 import com.cfs.bean.EmployeeBean;
 
-
-
 public class Model {
+	private CustomerDAO customerDAO;
 	private EmployeeDAO employeeDAO;
 	private FundDAO fundDAO;
 
 	public Model(ServletConfig config) throws ServletException {
 		try {
 			String jdbcDriver = config.getInitParameter("jdbcDriverName");
-			String jdbcURL    = config.getInitParameter("jdbcURL");
-			
+			String jdbcURL = config.getInitParameter("jdbcURL");
+
 			ConnectionPool pool = new ConnectionPool(jdbcDriver, jdbcURL);
-			employeeDAO  = new EmployeeDAO( "employee_table",pool); // Please update the  table name
-			fundDAO= new FundDAO("fund_table",pool);
-			
-			if(employeeDAO.getCount()==0){
+			customerDAO = new CustomerDAO("customers", pool);
+			employeeDAO = new EmployeeDAO("employees", pool);
+			fundDAO = new FundDAO("funds", pool);
+
+			if (employeeDAO.getCount() == 0) {
 				createDefaultUser();
 			}
-			
+
 		} catch (DAOException e) {
 			throw new ServletException(e);
 		} catch (RollbackException e) {
@@ -35,19 +35,26 @@ public class Model {
 			e.printStackTrace();
 		}
 	}
-	
-	public EmployeeDAO getEmployeeDAO() { return employeeDAO; }
-	public FundDAO getFundDAO() { return fundDAO; }
 
-	public void createDefaultUser() throws RollbackException{
-		 // create the admin login
+	public EmployeeDAO getEmployeeDAO() {
+		return employeeDAO;
+	}
+
+	public FundDAO getFundDAO() {
+		return fundDAO;
+	}
+
+	public CustomerDAO getCustomerDAO() {
+		return this.customerDAO;
+	}
+
+	public void createDefaultUser() throws RollbackException {
+		// create the admin login
 		EmployeeBean e1 = new EmployeeBean();
-		   e1.setUsername("admin");
-		   e1.setPassword("admin");
-		   e1.setFirstname("admin");
-		   e1.setLastname("admin");
-		   employeeDAO.createAutoIncrement(e1);
-		  		   
-		   
+		e1.setUsername("admin");
+		e1.setPassword("admin");
+		e1.setFirstname("admin");
+		e1.setLastname("admin");
+		employeeDAO.createAutoIncrement(e1);
 	}
 }
