ac14113d6f81ca24aaf068b1bb19cf2fd11fbb07
dearlike <13671369607@163.com>
2015-01-20 16:02:29 -0500

    SearchCustomerAction
    search customer by id

---
+++ src/com/cfs/controller/SearchCustomerAction.java
@@ -0,0 +1,87 @@
+package com.cfs.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.genericdao.MatchArg;
+import org.genericdao.RollbackException;
+import org.mybeans.form.FormBeanException;
+import org.mybeans.form.FormBeanFactory;
+
+import com.cfs.bean.CustomerBean;
+import com.cfs.bean.FundBean;
+import com.cfs.dao.CustomerDAO;
+import com.cfs.dao.FundDAO;
+import com.cfs.dao.Model;
+import com.cfs.form.CustomerForm;
+import com.cfs.form.SearchCustomerForm;
+import com.cfs.controller.*;
+
+
+
+public class SearchCustomerAction extends Action {
+	private FormBeanFactory<SearchCustomerForm> formBeanFactory = FormBeanFactory.getInstance(SearchCustomerForm.class);
+private CustomerDAO customerDAO;
+	
+	public SearchCustomerAction(Model model) {
+		
+		customerDAO= model.getCustomerDAO();
+}
+	
+	public String getName() {return "search_customer.do";}
+	
+	public String perform (HttpServletRequest request){
+		
+		List<String> errors = new ArrayList<String>();
+		
+		request.setAttribute("errors",errors);
+		
+		
+	    try {
+        	if (customerDAO.getCustomers() == null || CustomerDAO.getCustomers().length == 0) {
+            	return "login.do";
+            }
+        	request.setAttribute("customerList",customerDAO.getCustomers());
+        	
+        //	request.setAttribute("customerID", customerDAO.getCustomerID());
+        	
+        //	request.setAttribute("customerName", customerDAO.getCustomerName());
+        	
+	        SearchCustomerForm form = formBeanFactory.create(request);
+	       // request.setAttribute("form",form);
+
+	      //  if (!form.isPresent()) {
+	       //     return "index.jsp";
+	      //  }
+	
+	        // Any validation errors?
+	        errors.addAll(form.getValidationErrors());
+	        if (errors.size() != 0) {
+	            return "error.jsp";
+	        }
+	
+	        CustomerBean[] array = customerDAO.match(MatchArg.equals("customer_id", form.getCustomerID()));
+	        
+	     
+	       
+        	
+			CustomerBean customer = null;
+			
+        
+	        //HttpSession session = request.getSession(false);//why add false at here?
+	       // session.setAttribute("customer",customer);
+	        
+			request.setAttribute("searchResult", array);
+        } catch (FormBeanException e) {
+        	errors.add(e.getMessage());
+        	return "error.jsp";
+        } catch (RollbackException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    return "searchResult.jsp";
+	}
+}
---
+++ src/com/cfs/form/SearchCustomerForm.java
@@ -0,0 +1,17 @@
+package com.cfs.form;
+
+import org.mybeans.form.FormBean;
+
+public class SearchCustomerForm extends FormBean{
+
+	public Object getCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getCustomerID() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+}
