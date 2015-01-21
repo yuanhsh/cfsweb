81cdaba24b9514a7a138c36da4a5adbc16552dca
dearlike <13671369607@163.com>
2015-01-20 19:51:17 -0500

    search customer form

--- src/com/cfs/form/SearchCustomerForm.java
+++ src/com/cfs/form/SearchCustomerForm.java
@@ -1,17 +1,68 @@
 package com.cfs.form;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.mybeans.form.FormBean;
 
 public class SearchCustomerForm extends FormBean{
+	private String customers;
+	private String customerID;
+	private String button;
+	
+		public void setCustomers(String s) {
+		customers = s.trim();
+	}
 
-	public Object getCustomers() {
+	public void setCustomerID(String s) {
+		customerID = s.trim();
+	}
+	
+	public String getCustomers() {
 		// TODO Auto-generated method stub
-		return null;
+		return customers;
 	}
 
 	public String getCustomerID() {
 		// TODO Auto-generated method stub
-		return "";
+		return customerID;
+	}
+	public void setButton(String s) {
+		button = s;
+	}
+	public String getButton() {
+		return button;
+	}
+
+
+	public List<String> getValidationErrors() {
+		List<String> errors = new ArrayList<String>();
+
+		if (customers == null || customers.length() == 0) {
+			errors.add("Customer's name is required");
+		}
+
+		if (customers.matches(".*[<>\"].*")) {
+			errors.add("Customer's name cannot contain angular brackets or quotes");
+		}
+
+		if (customerID == null || customerID.length() == 0) {
+			errors.add("customerID is required");
+		}
+
+		if (button == null) {
+			errors.add("Clicking on search button is required");
+		}
+
+		if (!button.equals("Search")) {
+			errors.add("Invalid button");
+		}
+
+		if (errors.size() > 0) {
+			return errors;
+		}
+
+		return errors;
 	}
 
 }
