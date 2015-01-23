09ca5a3a2ab2a4ec566464ee0c6463aeb7d9d25d
Yuan Haisheng <yuanhaisheng@bit-ware.co.jp>
2015-01-23 13:52:38 -0500

    login change

--- src/com/cfs/controller/LoginAction.java
+++ src/com/cfs/controller/LoginAction.java
@@ -56,7 +56,6 @@ public class LoginAction extends Action {
 				request.setAttribute("employee", employeeDAO.getEmployeeByUsername(form.getUsername()));
 			}
 
-			// LoginForm form = formBeanFactory.create(request);
 			request.setAttribute("form", form);
 			if (!form.isPresent()) {
 				return "login.jsp";
@@ -70,31 +69,18 @@ public class LoginAction extends Action {
 			// User user = userDAO.read(form.getUsername());
 			CustomerBean customerBean = customerDAO.getCustomer(form.getUsername(), form.getPassword());
 			EmployeeBean employeeBean = employeeDAO.getEmployee(form.getUsername(), form.getPassword());
-			System.out.println(employeeBean + " emp ");
-			System.out.println(loginAs + " loginAs ");
-			
-
-			/*
-			 * Object loggedInUser = null; if (loginAs.equals("cust")) {
-			 * loggedInUser = customerDAO.read(form.getPk()); } else if
-			 * (loginAs.equals("emp")) { loggedInUser =
-			 * employeeDAO.read(form.getPk()); }
-			 */
 
 			if (customerBean == null && employeeBean == null) {
-				System.out.println("null value printed in if");
 				errors.add("Username or Password is incorrect");
 				return "login.jsp";
 			}
 
 			if (loginAs.equals("cust")) {
-				System.out.println("in cust");
 				if (customerBean!=null &&!customerBean.checkPassword(form.getPassword())) {
 					errors.add("Username or Password is incorrect");
 					return "login.jsp";
 				}
 			} else if (loginAs.equals("emp")) {
-				System.out.println("in emp");
 				if (employeeBean!=null && !employeeBean.checkPassword(form.getPassword())) {
 					errors.add("Username or Password is incorrect");
 					return "login.jsp";
@@ -108,16 +94,11 @@ public class LoginAction extends Action {
 				session.setAttribute("username", customerBean.getUsername());
 				session.setAttribute("customer_id", customerBean.getCustomer_id());
 				return "view_protfolio.do?customer_id="+customerBean.getCustomer_id();
-				//return "view-account-details.do?customer_id="+customerBean.getCustomer_id();
 			} else if (loginAs.equals("emp")) {
 				session.setAttribute("employee", employeeBean);
 				session.setAttribute("loginAs", "emp");
 				session.setAttribute("username", employeeBean.getUsername());
-				return "fund-list.do";
-				//////redirecting the employee on which page after login
-				//session.setAttribute("customer_id", customerBean.getCustomer_id());
-				//return "create-customer-account.do?employee_id="+employeeBean.getEmployee_id();/////////////////////////////////////////////////needs to be decided
-				//return "view-account-details.do?customer_id="+employeeBean.getEmployee_id();
+				return "search_fund.do";
 			}
 			return "login.jsp"; 
 		} catch (RollbackException e) {
