9e883e68624d09a279b2d6d7e5d69385ebdc9bf6
Yuan Haisheng <yuanhaisheng@bit-ware.co.jp>
2015-01-29 20:01:48 -0500

    deposit amount check

--- src/com/cfs/form/DepositCheckForm.java
+++ src/com/cfs/form/DepositCheckForm.java
@@ -44,6 +44,11 @@ public class DepositCheckForm extends FormBean {
 		long result = 0;
 		try {
 			double amount1 = Double.parseDouble(amount);
+			if(amount1 > 10000000.0) {
+				String msg = "You can't deposit amount larger than $10,000,000.00";
+				errors.add(msg);
+				throw new Exception(msg);
+			}
 			double total = amount1 * 100;
 			result = (long)total;
 			if(total != result || result <= 0) {
